9cb0a43be0950721b6f09c36e4530f4dc4d2e008
ramusee <ram.u@live.com>
2022-01-02 20:51:51 +0400

    Refactoring process

--- js/view.js
+++ js/view.js
@@ -1,48 +1,67 @@
-const elements = {
-  sectionHigh: document.querySelector('.priority_high'),
-  sectionLow: document.querySelector('.priority_low'),
-  tasksHigh: document.querySelector('.todo__tasks_high'),
-  tasksLow: document.querySelector('.todo__tasks_low'),
+const UI_ELEMENTS = {
+  SECTION_HIGH: document.querySelector('.priority_high'),
+  SECTION_LOW: document.querySelector('.priority_low'),
+  TASKS_HIGH: document.querySelector('.todo__tasks_high'),
+  TASKS_LOW: document.querySelector('.todo__tasks_low'),
+  FORM: document.querySelectorAll('.todo__add-task')
 }
-elements.sectionHigh.children[1][1].addEventListener('click', addTaskHigh)
-elements.sectionLow.children[1][1].addEventListener('click', addTaskLow)
+const BTN_ADD_HIGH = UI_ELEMENTS.SECTION_HIGH.children[1][1]
+const BTN_ADD_LOW = UI_ELEMENTS.SECTION_LOW.children[1][1]
+BTN_ADD_HIGH.addEventListener('click', addTaskHigh)
+BTN_ADD_LOW.addEventListener('click', addTaskLow)
+
 function addTaskHigh() {
-  const inputValue = elements.sectionHigh.children[1][0].value
-  if (inputValue !== '') {
-    elements.tasksHigh.insertAdjacentHTML(
+  const INPUT_VALUE = UI_ELEMENTS.SECTION_HIGH.children[1][0].value
+  if (INPUT_VALUE !== '') {
+    UI_ELEMENTS.TASKS_HIGH.insertAdjacentHTML(
       'afterbegin',
       `<div class="todo__task">
       <input class="task__check-input" type="checkbox">
       <span class="task__checkbox"></span>
-      <p class="task__text">${inputValue}</p>
+      <p class="task__text">${INPUT_VALUE}</p>
       <button type="button" class="button button_del"><img src="images/close-icon.svg" alt="delete" class="img_del"></button>
       </div>`
     )
-    const removeBtns = document.querySelectorAll('.button_del')
-    for (let btn of removeBtns) {
-      const task = btn.parentElement
-      btn.addEventListener('click', () => task.remove())
-    }
   }
-  const input = elements.sectionHigh.children[1][0]
-  console.log(input)
+    const BTNS_REMOVE = document.querySelectorAll('.button_del')
+    for (let btn of BTNS_REMOVE) {
+      btn.addEventListener('click', deleteTask)
+    }
+  resetForm()
+  const 
 }
 function addTaskLow() {
-  const inputValue = elements.sectionLow.children[1][0].value
-  if (inputValue !== '') {
-    elements.tasksLow.insertAdjacentHTML(
+  const INPUT_VALUE = UI_ELEMENTS.SECTION_LOW.children[1][0].value
+  if (INPUT_VALUE !== '') {
+    UI_ELEMENTS.TASKS_LOW.insertAdjacentHTML(
       'afterbegin',
       `<div class="todo__task">
       <input class="task__check-input" type="checkbox">
       <span class="task__checkbox"></span>
-      <p class="task__text">${inputValue}</p>
+      <p class="task__text">${INPUT_VALUE}</p>
       <button type="button" class="button button_del"><img src="images/close-icon.svg" alt="delete" class="img_del"></button>
       </div>`
     )
-    const removeBtns = document.querySelectorAll('.button_del')
-    for (let btn of removeBtns) {
-      const task = btn.parentElement
-      btn.addEventListener('click', () => task.remove())
-    }
   }
+  const BTNS_REMOVE = document.querySelectorAll('.button_del')
+  for (let btn of BTNS_REMOVE) {
+    btn.addEventListener('click', deleteTask)
+  }
+  resetForm();
+}
+
+function changeStatus() {
+  
+  this.children[0].checked === true ? this.style.backgroundColor = 'black' : this.style.backgroundColor = '#fff'
+  console.log(this.children[0])
+}
+
+function resetForm() {
+  for(let form of UI_ELEMENTS.FORM) {
+    form.reset();
+  }
+}
+
+function deleteTask() {
+  this.parentElement.remove()
 }
