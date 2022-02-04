03fdcdf112a6144cf4392cbd46dc98dda87dce1c
ramusee <ram.u@live.com>
2022-02-04 22:09:07 +0400

    added date

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-import { UI_ELEMENTS } from './view.js'
+import { UI_ELEMENTS } from './view.js';
+import { format } from 'date-fns';
 
-const BTN_ADD_HIGH = UI_ELEMENTS.SECTION_HIGH.children[1][1]
-const BTN_ADD_LOW = UI_ELEMENTS.SECTION_LOW.children[1][1]
-BTN_ADD_HIGH.addEventListener('click', addTaskHigh)
-BTN_ADD_LOW.addEventListener('click', addTaskLow)
+const BTN_ADD_HIGH = UI_ELEMENTS.SECTION_HIGH.children[1][1];
+const BTN_ADD_LOW = UI_ELEMENTS.SECTION_LOW.children[1][1];
+BTN_ADD_HIGH.addEventListener('click', addTaskHigh);
+BTN_ADD_LOW.addEventListener('click', addTaskLow);
 
 function addTaskHigh() {
-  const INPUT_VALUE = UI_ELEMENTS.SECTION_HIGH.children[1][0].value
+  const INPUT_VALUE = UI_ELEMENTS.SECTION_HIGH.children[1][0].value;
+  const nowDate = format(new Date(), 'dd.MM.yyyy');
   if (INPUT_VALUE !== '') {
     UI_ELEMENTS.TASKS_HIGH.insertAdjacentHTML(
       'afterbegin',
@@ -14,22 +16,27 @@ function addTaskHigh() {
       <input class="task__check-input" type="checkbox">
       <span class="task__checkbox"></span>
       <p class="task__text"></p>
-      <button type="button" class="button button_del"><img src="images/close-icon.svg" alt="delete" class="img_del"></button>
+      <div class="task__container">
+      <span class="task__date">${nowDate}</span>
+      <button type="button" class="button button_del">
+      </button>
+      </div>
       </div>`
-    )
+    );
   }
-  const tasks = document.querySelectorAll('.task__text')
+  const tasks = document.querySelectorAll('.task__text');
   tasks.forEach((task) => {
-    if (task.textContent === '') task.textContent = INPUT_VALUE
-  })
-  const BTNS_REMOVE = document.querySelectorAll('.button_del')
-  BTNS_REMOVE.forEach((btn) => btn.addEventListener('click', deleteTask))
-  const CHECK_INPUTS = document.querySelectorAll('.task__check-input')
-  CHECK_INPUTS.forEach((item) => item.addEventListener('click', changeStatus))
-  UI_ELEMENTS.FORM_HIGH.reset()
+    if (task.textContent === '') task.textContent = INPUT_VALUE;
+  });
+  const BTNS_REMOVE = document.querySelectorAll('.button_del');
+  BTNS_REMOVE.forEach((btn) => btn.addEventListener('click', deleteTask));
+  const CHECK_INPUTS = document.querySelectorAll('.task__check-input');
+  CHECK_INPUTS.forEach((item) => item.addEventListener('click', changeStatus));
+  UI_ELEMENTS.FORM_HIGH.reset();
 }
 function addTaskLow() {
-  const INPUT_VALUE = UI_ELEMENTS.SECTION_LOW.children[1][0].value
+  const INPUT_VALUE = UI_ELEMENTS.SECTION_LOW.children[1][0].value;
+  const nowDate = format(new Date(), 'dd.MM.yyyy');
   if (INPUT_VALUE !== '') {
     UI_ELEMENTS.TASKS_LOW.insertAdjacentHTML(
       'afterbegin',
@@ -37,25 +44,29 @@ function addTaskLow() {
       <input class="task__check-input" type="checkbox">
       <span class="task__checkbox"></span>
       <p class="task__text"></p>
-      <button type="button" class="button button_del"><img src="images/close-icon.svg" alt="delete" class="img_del"></button>
+      <div class="task__container">
+      <span class="task__date">${nowDate}</span>
+      <button type="button" class="button button_del">
+      </button>
+      </div>
       </div>`
-    )
+    );
   }
-  const tasks = document.querySelectorAll('.task__text')
+  const tasks = document.querySelectorAll('.task__text');
   tasks.forEach((task) => {
-    if (task.textContent === '') task.textContent = INPUT_VALUE
-  })
-  const BTNS_REMOVE = document.querySelectorAll('.button_del')
-  BTNS_REMOVE.forEach((btn) => btn.addEventListener('click', deleteTask))
-  const CHECK_INPUTS = document.querySelectorAll('.task__check-input')
-  CHECK_INPUTS.forEach((item) => item.addEventListener('click', changeStatus))
-  UI_ELEMENTS.FORM_LOW.reset()
+    if (task.textContent === '') task.textContent = INPUT_VALUE;
+  });
+  const BTNS_REMOVE = document.querySelectorAll('.button_del');
+  BTNS_REMOVE.forEach((btn) => btn.addEventListener('click', deleteTask));
+  const CHECK_INPUTS = document.querySelectorAll('.task__check-input');
+  CHECK_INPUTS.forEach((item) => item.addEventListener('click', changeStatus));
+  UI_ELEMENTS.FORM_LOW.reset();
 }
 function changeStatus() {
   this.parentElement.children[0].checked === true
     ? (this.parentElement.style.backgroundColor = '#f4f4f4')
-    : (this.parentElement.style.backgroundColor = '#fff')
+    : (this.parentElement.style.backgroundColor = '#fff');
 }
 function deleteTask() {
-  this.parentElement.remove()
+  this.parentElement.parentElement.remove();
 }
